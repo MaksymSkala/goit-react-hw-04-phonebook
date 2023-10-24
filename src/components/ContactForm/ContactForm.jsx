79d1098beca19c9656d3e2f13c3a57eb35d8ac2c
MaksymSkala <maksymskala@gmail.com>
2023-10-25 01:30:41 +0300

    Add

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,58 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './ContactForm.css';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+const ContactForm = ({ onAddContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const { name, number } = this.state;
 
     if (!name || !number) {
       alert('Please fill in all fields.');
       return;
     }
 
-    this.props.onAddContact(name, number);
+    onAddContact(name, number);
 
-    this.setState({ name: '', number: '' });
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { name, number } = this.state;
-
-    return (
-      <form className='form' onSubmit={this.handleSubmit}>
-        <label className='form-input'>
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label className='form-input'>
-          Number:
-          <input
-            type="tel"
-            name="number"
-            value={number}
-            onChange={this.handleChange}
-          />
-        </label>
-        <button className='form-button' type="submit">Add contact</button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className='form' onSubmit={handleSubmit}>
+      <label className='form-input'>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </label>
+      <label className='form-input'>
+        Number:
+        <input
+          type="tel"
+          value={number}
+          onChange={(event) => setNumber(event.target.value)}
+        />
+      </label>
+      <button className='form-button' type="submit">Add contact</button>
+    </form>
+  );
+};
 
 export default ContactForm;
